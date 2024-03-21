1d2a250ffe19ca9b055b35f9f8cf8c065d0bb288
Ethan Metz <76500833@protonmail.com>
2024-03-21 16:35:09 -0700

    configured webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,43 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new InjectManifest({
+        swSrc: './src/sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Your App Name',
+        short_name: 'App',
+        description: 'Your App Description',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        start_url: '/',
+        icons: [
+          {
+            src: path.resolve('src/assets/images/icons/android-chrome-192x192.png'),
+            sizes: [192, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
